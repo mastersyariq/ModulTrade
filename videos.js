bb0a5c7c78384c39d40900b58228599ab47260a0
mastersyariq <mastersyariq@gmail.com>
2026-04-06 21:47:30 +0700

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,477 @@
+//list video
+
+var VIDEO_LIST = [
+  {
+    link:  "https://drive.google.com/file/d/1qP04nwi61iz6KOAlBsZlVMVivjsaWw9h/view",
+    title: "01. Cara Melakukan Trend Analysis",
+    tags:  ["CRYPTO TRADING", "BAB 1", "TECHNICAL RESEARCH"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qL2dVKUkYOyk7O-MBXl0wh3vWbpUpc5s/view",
+    title: "02. Cara Melakukan Area Analysis",
+    tags:  ["CRYPTO TRADING", "BAB 1", "TECHNICAL RESEARCH"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qEIIhTXd5i_plqGEVzLSp7O35kTikkWT/view",
+    title: "03. Cara Melakukan Pattern Analaysis",
+    tags:  ["CRYPTO TRADING", "BAB 1", "TECHNICAL RESEARCH"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qBX7CYQjJldoA-fg6bmEhJzy5_gbWdkI/view",
+    title: "04. Cara Melakukan Trigger Analysis",
+    tags:  ["CRYPTO TRADING", "BAB 1", "TECHNICAL RESEARCH"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qDckl9-1IWihasSpZVMZQArBKN-hHDm6/view",
+    title: "05. Cara Melakukan Entry Exit Analysis",
+    tags:  ["CRYPTO TRADING", "BAB 1", "TECHNICAL RESEARCH"]
+  },
+
+  
+  {
+    link:  "https://drive.google.com/file/d/1fwpac5U4Z6L1LOtNmNTYcPTiZ894H9lT/view",
+    title: "01. Mind Management",
+    tags:  ["CRYPTO TRADING", "BAB 2", "Mind Management"]
+  },
+
+  
+  {
+    link:  "https://drive.google.com/file/d/1fkoexJEF887g_5Dhx-VcKbW0HkK0To7n/view",
+    title: "01. Money Management",
+    tags:  ["CRYPTO TRADING", "BAB 3", "Money Management"]
+  },
+
+  {
+    link:  "https://drive.google.com/file/d/1fkoexJEF887g_5Dhx-VcKbW0HkK0To7n/view",
+    title: "01. Cara Screening Altcoin Dari 0",
+    tags:  ["CRYPTO TRADING", "BAB 4", "Screening Altkoin"]
+  },
+
+  {
+    link:  "https://drive.google.com/file/d/1ikg1vZL_RW1fENmsAGxfdFiWf_bFV-R6/view",
+    title: "01. Intro To Fundamental Research",
+    tags:  ["CRYPTO TRADING", "BAB 5", "Fundamental Research"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1inU_RcnebpcPJkUtfpB6MgiXRxSTja9h/view",
+    title: "02. Faktor yang Berperan Dalam Fundamental Research",
+    tags:  ["CRYPTO TRADING", "BAB 5", "Fundamental Research"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1ir-Eha5r1QxleNsTFbJdAajzot_jxn9Z/view?usp=drive_link",
+    title: "03. Mendalami Tokonomics Project",
+    tags:  ["CRYPTO TRADING", "BAB 5", "Fundamental Research"]
+  },
+
+  {
+    link:  "https://drive.google.com/file/d/1isXxwJ7vndk5simuyafoGzyIkN3rrVQt/view?usp=drive_link",
+    title: "01. Intro to Narrative Analyst",
+    tags:  ["CRYPTO TRADING", "BAB 6", "Narrative Research"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1ivpoCwsVPjYuKfDtA9aAGlJCy_g70RIU/view?usp=drive_link",
+    title: "02. How to be Early Narrative",
+    tags:  ["CRYPTO TRADING", "BAB 6", "Narrative Research"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1ivqmj2d8B0fdda9h_iHugP0HZKXvo3cd/view?usp=drive_link",
+    title: "03. Portofolio Allocation",
+    tags:  ["CRYPTO TRADING", "BAB 6", "Narrative Research"]
+  },
+
+  {
+    link:  "https://drive.google.com/file/d/1iM5EWAXHrCBqXBDA3PDMk81A7dG_ak2Q/view?usp=drive_link",
+    title: "01. Apa itu Harmonic Trading",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1iPv0RmNitI6ACFOOUoNdvUId6YelHtW1/view?usp=drive_link",
+    title: "02. Tahapan Menerapkan Harmonic",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1iRWPD7TlnxDI6q87MHf8ZbRA-IbD9D2e/view?usp=drive_link",
+    title: "03. Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1iTw7xo0E48yB_7Yn2gjpRAKuuoHkhW_Y/view?usp=drive_link",
+    title: "04. Elliot Wave",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1iW7KmgFmErZz3rXLg7c0Uk3jfBBc4HEV/view?usp=drive_link",
+    title: "05. Korelasi Elliot Wave dengan Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1iWSNwDEx_qnSFEcVdkUPvrQDCjDpisrO/view?usp=drive_link",
+    title: "06. Dasar Dari Pola dn Rasio Harmonic Trading",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1iX7-G7_fBYbjvtAH2WWYlkWEeHA63E5_/view?usp=drive_link",
+    title: "07. Berbagai Harmonic Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1iYQ8Pk56FIsnD_SRVYBWCU3yZ9CFPbi2/view?usp=drive_link",
+    title: "08. Shark & Butterfly Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1i_aE3jOkhqn1QvfuZB8YgeA1qR55NEjg/view?usp=drive_link",
+    title: "09. Entry Trigger",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1icm1UyPKKF4AAf_1fAxk817PGCA0FBmc/view?usp=drive_link",
+    title: "10. Rangkuman dan Fakta dari Trading",
+    tags:  ["CRYPTO TRADING", "BAB 7", "Harmonic Trade"]
+  },
+
+  
+  {
+    link:  "https://drive.google.com/file/d/1ixPcY5x1cGO9MFbO-DZqe60THc2fNNTA/view?usp=drive_link",
+    title: "01. Apa itu Order Flow",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1ixW8BFsTzphN0UhATtZEJHVnTxlsabG2/view?usp=drive_link",
+    title: "02. Dasar Dari Order FLow",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1j-NfpNR5VsvGpLUpUOztwmrDT7ImTqTY/view?usp=drive_link",
+    title: "03. Cara melihat footprint",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1j6hOQivc9UFKvYMSxbETn3-TziWSqLP4/view?usp=drive_link",
+    title: "04. Cara Membaca Order Flow",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1j94jc01ncTX0VZlPGmApz0kZSTECdqbE/view?usp=drive_link",
+    title: "05. High Value Node dan Delta",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jB70531LpYg70pmPKK-l_7xVG1ELToI7/view?usp=drive_link",
+    title: "06. Volume Cluster",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jFDkyRez8X01Bzkt2B8ZNDAsWz_NuxQ0/view?usp=drive_link",
+    title: "07. Unfinished Bussiness dan Cumulative Delta",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jNRPiMz3haRF-aRa9OZVp2CmaG8nI-Ul/view?usp=drive_link",
+    title: "08. Penggunaan Atas",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jNzyVQHjn4XI0xkjuvnfFhAQ9h65UzvO/view?usp=drive_link",
+    title: "09. Techinical Indicator Dalam Atas",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jPaUgB1316sheQSUTI41wUq-bM5F1X30/view?usp=drive_link",
+    title: "10. Heatmap",
+    tags:  ["CRYPTO TRADING", "BAB 8", "Order Flow"]
+  },
+
+
+  {
+    link:  "https://drive.google.com/file/d/1evoveieRKm6As76YFSIIXQuaN7BgVcAZ/view?usp=drive_link",
+    title: "01. Apa itu Smart Money",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f3SNImbo-mDc0fnFppuxJQpTsySL_PxM/view?usp=drive_link",
+    title: "02. Apa Fokus dari Smart Money",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f4Y4v9Tl19Bt8d7ifYjJQIR4lx8FSHwy/view?usp=drive_link",
+    title: "03. Konsep Liquidty I",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f4xD9nijBm2MEXFN9aKzsjCNZBZFBKT1/view?usp=drive_link",
+    title: "04. Konsep Liquidty II",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f5kr--WqndNX5yIxTIKL-GvX6X9Q6NFa/view?usp=drive_link",
+    title: "05. Konsep Liquidty III",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f7orZPRUUHJtaHXwupkCAUqJaGV8t2zl/view?usp=drive_link",
+    title: "06. Market Structure",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f8B47ZgwbHlSZhQI-ldcdThrfz7taWWy/view?usp=drive_link",
+    title: "07. Change of Character",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f8NUUy03s3GN5F4AYz61TYS2ZP8h-W26/view?usp=drive_link",
+    title: "08. Order Block",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1f9yM3g_3lC16D07hQR9TSNgQHZ_TiAH7/view?usp=drive_link",
+    title: "09. Momentum",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1etbSpxpEZ_3VIAJ5UgxCoPK1Umg90Aot/view?usp=drive_link",
+    title: "10. Penerapan Smart Money",
+    tags:  ["CRYPTO TRADING", "BAB 9", "Smart Money"]
+  },
+
+  {
+    link:  "https://drive.google.com/file/d/1jQwkP8wNZo3CEKHmb9leHliUktFLn8ln/view?usp=drive_link",
+    title: "01. Apa itu Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jVk846_JhhQ0ZIoVCsW_L5yXJO5GC4k4/view?usp=drive_link",
+    title: "02. Keajaiban Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jXXGQyrThv_wor60kZncj04pQbSvC1RB/view?usp=drive_link",
+    title: "03. Golden Ratio Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jYGLC58Cc7HaD4SRcjj41d1FwBngccN-/view?usp=drive_link",
+    title: "04. Sistem Angka Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jQvuJargM6s5gWHvYWUuPW9xnyEHruZ-/view?usp=drive_link",
+    title: "05. Retracement Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jck2vyfdDmilcFqM9DnBCDMJCHUhK7pm/view?usp=drive_link",
+    title: "06. Harmonic Support Using Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jdmEpKQvnts-DosQROek4m0kXe4SPUs3/view?usp=drive_link",
+    title: "07. Harmonic Resistance Using Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jeJLnvrPG-D-qshQh8n3OsLm7MRqn3xP/view?usp=drive_link",
+    title: "08. Trend Based Fibonacci Extension",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1jhLjGe3qLmPpjdz2ACeQgP_Va0j64JJv/view?usp=drive_link",
+    title: "09. Fibonacci Channels",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1joTYlY16uzbosVxuAKC4jboffjJ_Ys0v/view?usp=drive_link",
+    title: "10. Pendalaman Fibonacci",
+    tags:  ["CRYPTO TRADING", "BAB 10", "Fibonacci Secret"]
+  },
+  
+  {
+    link:  "https://drive.google.com/file/d/1fHyRyGKgI0QHpYW1b2ajITRATVS1bW_6/view?usp=drive_link",
+    title: "01. Technical Indicator",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fHG3w9nWL3oqkBdHR0RzHYAh2urzWow7/view?usp=drive_link",
+    title: "02. Pivot Point",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fKfIbx3jnWiD0HlTBawCOsirycuCZ8Iv/view?usp=drive_link",
+    title: "03. Keltner Channel",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fLNCVHsy5g23cp8A7SLddxSwy2djSk3n/view?usp=drive_link",
+    title: "04. Parabolic SAR",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fLzT1pRSN71AlBpWXFbWcR7iN8xsYsIE/view?usp=drive_link",
+    title: "05. Renko Chart",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fT463H0gQKPAYaHu43aJLI4y85iCJypx/view?usp=drive_link",
+    title: "06. Know Sure Thing",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fXKR941Y2mhj3LZAwqaQfmBElpZwU7i6/view?usp=drive_link",
+    title: "07. Donchain Channel",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fYhLqKDgZzCS-Wzktj-ggr-QEhFxtWr2/view?usp=drive_link",
+    title: "08. Chaikin Money Flow",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fYoMYHGRtCyiUm9BaZxeA6YERkZBr9U3/view?usp=drive_link",
+    title: "09. Icimoku Cloud",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1fGe4gzGOWJKc12mIt1dLsnznaE1V49OR/view?usp=drive_link",
+    title: "10. Volume Profile Fix Range",
+    tags:  ["CRYPTO TRADING", "BAB 11", "Technical Indicator"]
+  },
+
+  {
+    link:  " ",
+    title: "01. Apa itu Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "02. ABCD Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "03. Bat & Spider Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "04. Crab Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "05. Gartly Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "06. Diamond Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "07. Boardening Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "08. Andrew Pitchfork Pattern",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "09. Linear Regression Channel",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+  {
+    link:  " ",
+    title: "10. Rectangle Channels",
+    tags:  ["CRYPTO TRADING", "BAB 12", "Crypto Pattern"]
+  },
+
+  {
+    link:  "https://drive.google.com/file/d/1qIlm3aw1CrtI6mLv_bWQMshegcMK23eh/view?usp=drive_link",
+    title: "01. Faktor Psikologi",
+    tags:  ["CRYPTO TRADING", "BAB 13", "Crypto Psychology"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qJMZGQlXTMeOXRpGwI05LycFFCHj9qkx/view?usp=drive_link",
+    title: "02. Psikologi Ketika Trading & Investasi",
+    tags:  ["CRYPTO TRADING", "BAB 13", "Crypto Psychology"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qJPha8EgqaZmJ61o0Z92nrR5EmYVXOfR/view?usp=drive_link",
+    title: "03. Mekanisme Dalam Otak Yang Menyebabkan Bias",
+    tags:  ["CRYPTO TRADING", "BAB 13", "Crypto Psychology"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qLxNmeGb4rWtGOZ3OXUwoYqx9lxRBaf0/view?usp=drive_link",
+    title: "04. Pola Perilaku Para Trader & Investor",
+    tags:  ["CRYPTO TRADING", "BAB 13", "Crypto Psychology"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qPkMBni-J5MwFDAHk_E-OuzNmLlVyBKI/view?usp=drive_link",
+    title: "05. Overconfidence Bias",
+    tags:  ["CRYPTO TRADING", "BAB 13", "Crypto Psychology"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qS2FU0clgXRJjIO4GSWMpnWtjva9_B4W/view?usp=drive_link",
+    title: "06. Herding Behavior Bias",
+    tags:  ["CRYPTO TRADING", "BAB 13", "Crypto Psychology"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1qSsP49-f9lAmr1rR7FcLEPjB_HJzuYUb/view?usp=drive_link",
+    title: "07. Loss Aversion Bias",
+    tags:  ["CRYPTO TRADING", "BAB 13", "Crypto Psychology"]
+  },
+  
+
+  {
+    link:  "https://drive.google.com/file/d/1g2Xik-hleq4ez0THXfOExymMqmgcXC78/view?usp=drive_link",
+    title: "01. Sejarah Wyckoff Trading Startegy",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1g2Y_9Qx_S5_vT2On2AW4D4ZzsTTzam5x/view?usp=drive_link",
+    title: "02. Hukum Dan Prinsip Dari Wyckoff",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1g4HeKu_yCkVZL0JS0ri-DePcTDb9we8k/view?usp=drive_link",
+    title: "03. Penerapan Fase Dalam Market Secara Umum",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1g4PP4bOOu0ilSYZ6ewG_x6k9A6KA_h-i/view?usp=drive_link",
+    title: "04. Berbagai Fase Wyckoff Dalam Trading Crypto",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1g8IpuZ6V_3SdVWm6QbqjIf1JJadHj271/view?usp=drive_link",
+    title: "05. Supply Dan Demand",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1gD8-CSrSAFe8ScTPdajLBojjv0n5mw4J/view?usp=drive_link",
+    title: "06. Volume Spread Analysis",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1gGJGGWqp8vtPVSSaggN8xUS0_X0PpEF1/view?usp=drive_link",
+    title: "07. Pergerakan Price Action Sederhana",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1gGmiiRgasURPG6ObAGtbZcyV0_MKjxnM/view?usp=drive_link",
+    title: "08. Fase Dari Wyckoff Part I",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1gGpRdMTLPhdcM53KI7Ue9qopJd-nf7jJ/view?usp=drive_link",
+    title: "09. Fase Dari Wyckoff Part II",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+  {
+    link:  "https://drive.google.com/file/d/1g-2BJVO2tQ04dEKPYcgqQDXOPB_UnRxi/view?usp=drive_link",
+    title: "10. Penerapan Wyckoff Dalam Crypto",
+    tags:  ["CRYPTO TRADING", "BAB 14", "Crypto Wyckoff Trade"]
+  },
+];
